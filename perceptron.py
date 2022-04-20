5adb00c300ea6d367b04c4df2eb3945a71aa04f0
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-04-20 21:22:22 +0100

    add w and net arrays

--- perceptron.py
+++ perceptron.py
@@ -1,20 +1,20 @@
 import numpy as np
-
-
-
 x3=np.random.random_integers(7,size=(4,2))
-
-
+w=np.random.random((4,2))
 x4=map( lambda x:x[0]+x[1],x3)
+net=map( lambda x,w:[x[0]*w[0],x[1]*w[1]],x3,w)
+print(x3)
+print(w)
+print(net)
 
-print(x4)
+
 y=[];
 for i in x4:
     if(i>5):
         y.append(1)
     else:
         y.append(-1)    
-print(y)
+
             
 
 
