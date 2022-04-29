0000919861e6f69c31f803dbbbf5e91314e54588
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-04-29 18:56:11 +0100

    qst 2.5

--- perceptron2.py
+++ perceptron2.py
@@ -1,20 +1,28 @@
 import numpy as np
 
-x1=np.array([[0.325,0.768,1],[0.798,0.821,1]
+Data_set=np.array([[0.325,0.768,1],[0.798,0.821,1]
              ,[0.364,0.417,1],[0.241,0.605,1]
              ,[0.653,0.488,1]])
-x2=[[0.325,0.768,1],[0.798,0.821,1]
-             ,[0.364,0.417,1],[0.241,0.605,1]
-             ,[0.653,0.488,1]]
-print(type(x2))
+      
+
+class Neuron :
+    alpha=0.01;
+    output=[];
+    def __init__(self,data) :
+        self.data=data;
+        self.bias=0.5;
+        self.wights=np.random.random();
     
-def sum_Y(list):
-    sum=map( lambda x:x[0]+x[1],x1)
-    y=[];
-    for i in sum:
-        if(i>1):
-            y.append(1)
-        else:
-            y.append(-1)
-    print(y)        
-sum_Y(x1)
+    def sum_output(self):
+         out=(self.data*self.wights)+self.bias;
+         for i in out:
+             if(i>0):
+                 self.output.append(1)
+             else:
+                 self.output.append(-1)
+             
+         
+        
+        
+        
+    
